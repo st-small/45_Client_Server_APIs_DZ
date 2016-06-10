47c022f02590a7ece089bf0659c292d26199da7a
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-06-10 12:32:07 +0300

    Добавил view followers

---
+++ 45_Client_Server_APIs_DZ/45_Client_Server_APIs_DZ/SiSFollowers.h
@@ -0,0 +1,13 @@
+//
+//  SiSFollowers.h
+//  45_Client_Server_APIs_DZ
+//
+//  Created by Stanly Shiyanovskiy on 10.06.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SiSFollowers : UIViewController
+
+@end
---
+++ 45_Client_Server_APIs_DZ/45_Client_Server_APIs_DZ/SiSSubscriptions.h
@@ -0,0 +1,13 @@
+//
+//  SiSSubscriptions.h
+//  45_Client_Server_APIs_DZ
+//
+//  Created by Stanly Shiyanovskiy on 10.06.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SiSSubscriptions : UIViewController
+
+@end
